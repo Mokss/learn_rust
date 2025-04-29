238db24a56781a3e5db93de19815f38171b77c11
Mokss <moksshuv@yandex.ru>
2025-04-29 23:43:54 +0300

    познакомился с shadowing переменных, не понравилось, выглядит как что то очень странное и не правильное, если новый тип данных, то должно быть новое семантически понятное имя переменной, а не перезаписывать тип старой

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // заметил, что пока я здесь не прописал импорт, у меня автокомлит в файле guess_game не работал
 // это вам не js, все начинается с main)
-mod guess_game;
+// mod guess_game;
+
+mod shadowing;
 
 fn main () {
-    guess_game::guess_game()
+    // guess_game::guess_game()
+    shadowing::shadowing()
 }
